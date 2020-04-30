6345829b6312cc597a7968dbee8e1a9ff48c66ee
nandini <nininandini@gmail.com>
2020-04-30 10:38:07 -0700

    Trasforming Data.

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
 
@@ -16,7 +17,16 @@ class Blog extends Component {
 
         axios.get("https://jsonplaceholder.typicode.com/posts")
         .then(response => {
-            this.setState({posts:response.data})
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post=>{
+               return {
+                ...post,
+                author:'Max'
+               } 
+
+            })
+
+            this.setState({posts:updatedPosts})
           // console.log(response);
         });
        
@@ -25,7 +35,7 @@ class Blog extends Component {
     render () {
 
         const posts=this.state.posts.map(post=>{
-            return   <Post key={post.id} title={post.title}/>
+            return   <Post key={post.id} title={post.title} author={post.author}/>
         }
 
         )

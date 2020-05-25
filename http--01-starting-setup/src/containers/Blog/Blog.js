85245da5d0786658449f322fd83dd6f9d9c28ff8
nandini <nininandini@gmail.com>
2020-05-25 10:35:13 -0700

    Setting up and rendering components for different pages.

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 //import axios from '../../axios';
+import {Route} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPosts from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -18,7 +20,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+               {/*<Route path="/" exact render={()=> <h1>Home</h1>}/>
+                <Route path="/" exact render={()=> <h1>Home 2</h1>}/>*/}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post"  component={NewPosts}/>
             </div>
         );
     }

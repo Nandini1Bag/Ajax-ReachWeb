cda14542f19695306e3b52a3048535eec6ec5e75
nandini <nininandini@gmail.com>
2020-05-27 10:34:41 -0700

    Passing and Extracting Route Parameters.

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -6,6 +6,7 @@ import {Route,NavLink} from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPosts from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -34,6 +35,7 @@ class Blog extends Component {
                 <Route path="/" exact render={()=> <h1>Home 2</h1>}/>*/}
                 <Route path="/" exact component={Posts}/>
                 <Route path="/new-post"  component={NewPosts}/>
+                <Route path="/:id" exact component={FullPost}/>
             </div>
         );
     }

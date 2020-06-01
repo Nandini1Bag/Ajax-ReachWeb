45990db2edcb999702e73c68af9c4800263c3aee
nandini <nininandini@gmail.com>
2020-06-01 10:57:37 -0700

    Loading Routes Lazily.

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -5,7 +5,11 @@ import {Route,NavLink,Switch,Redirect} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPosts from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPosts from './NewPost/NewPost';
+const AsyncNewPost=asyncComponent(()=>{
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
@@ -37,7 +41,7 @@ class Blog extends Component {
                {/*<Route path="/" exact render={()=> <h1>Home</h1>}/>
                 <Route path="/" exact render={()=> <h1>Home 2</h1>}/>*/}
                 <Switch>           
-                {this.state.auth ? <Route path="/new-post"  component={NewPosts}/>: null }
+                {this.state.auth ? <Route path="/new-post"  component={AsyncNewPost}/>: null }
                 <Route path="/posts"  component={Posts}/>
                 <Route render={()=> <h1>Not found</h1>}/>
                 {/*<Route path="/"  component={Posts}/>*/}
